37c5e9b894cb3d409461858e4f93c5eb2c899616
Akshat Gupta <akshatgtc@gmail.com>
2024-02-23 02:14:39 +0530

    commit

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0xcc08a718b9d8A31CF4F202493bd6fDE096B26579";
 export const contractABI = abi.abi;
